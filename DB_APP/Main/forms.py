f13d5c9d7c6b6b6687e09191d49caf36c0409e63
fschumacher <felix.schumacher@iee.fraunhofer.de>
2021-06-15 13:14:25 +0200

    DateField format european

--- DB_APP/Main/forms.py
+++ DB_APP/Main/forms.py
@@ -3,7 +3,7 @@ from wtforms.fields import StringField, IntegerField, FloatField, SubmitField, D
 from wtforms.validators import Length, DataRequired
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
 from DB_APP import db
-from DB_APP.Main.utils import articlenumgenerator, customernumgenerator, producernumgenerator
+from DB_APP.Main.utils import articlenumgenerator, customernumgenerator, producernumgenerator, ordernumgenerator
 from DB_APP.Main.models import Producer, Person, Order, Article
 
 
@@ -14,8 +14,9 @@ class ArticleForm(FlaskForm):
         query_factory=lambda: Producer.query.all(),
         get_label="producername",
     )
-    articlename = StringField()
-    price = FloatField()
+    articlename = StringField(label="Name", validators=[DataRequired(), Length(max=50)],
+                              render_kw={'placeholder': 'Name'})
+    price = FloatField(label="Preis", validators=[DataRequired()], render_kw={'placeholder': '0.00'})
 
     def save(self):
         article = Article(
@@ -41,7 +42,6 @@ class ArticleForm(FlaskForm):
 
 # Create new or Edit existing Orders
 class OrderForm(FlaskForm):
-    ordernumber = IntegerField()
     articlenumber = QuerySelectField(
         label="Artikel",
         query_factory=lambda: Article.query.all(),
@@ -52,11 +52,11 @@ class OrderForm(FlaskForm):
         query_factory=lambda: Person.query.all(),
         # get_label="personnumber",
     )
-    articlequantity = IntegerField()
-    ordersum = IntegerField()
+    articlequantity = IntegerField(label="Anzahl", render_kw={'placeholder': 'Anzahl'})
+    ordersum = IntegerField(label="Bestellsumme", render_kw={'placeholder': 'Bestellsumme'})
 
     def save(self):
-        order = Order(ordernumber=self.ordernumber.data,
+        order = Order(ordernumber=ordernumgenerator(),
                       articlenumber=self.articlenumber.data.articlenumber,
                       customernumber=self.customernumber.data.customernumber,
                       articlequantity=self.articlequantity.data,
@@ -81,9 +81,9 @@ class OrderForm(FlaskForm):
 
 # Create new or Edit existing Producers
 class ProducerForm(FlaskForm):
-    producernumber = IntegerField()
-    producername = StringField()
-    country = StringField()
+    producername = StringField(label="Name", validators=[DataRequired(), Length(max=30)],
+                               render_kw={'placeholder': 'name'})
+    country = StringField(label="Land", validators=[DataRequired(), Length(max=30)], render_kw={'placeholder': 'Land'})
 
     def save(self):
         producer = Producer(producernumber=producernumgenerator(),
@@ -105,14 +105,17 @@ class ProducerForm(FlaskForm):
 
 # Create new or Edit existing Persons
 class PersonForm(FlaskForm):
-    # personnumber = IntegerField()
-    lastname = StringField()
-    firstname = StringField()
-    street = StringField()
-    place = StringField()
-    zipcode = IntegerField()
-    country = StringField()
-    birthday = StringField()
+    lastname = StringField(label="Nachname", validators=[DataRequired(), Length(max=20)],
+                           render_kw={'placeholder': 'Nachname'})
+    firstname = StringField(label="Vorname", validators=[DataRequired(), Length(max=20)],
+                            render_kw={'placeholder': 'Vorname'})
+    street = StringField(label="Straße", validators=[DataRequired(), Length(max=30)],
+                         render_kw={'placeholder': 'Musterstraße 1'})
+    place = StringField(label="Ort", validators=[DataRequired(), Length(max=30)],
+                        render_kw={'placeholder': 'Musterort'})
+    zipcode = IntegerField(label="PLZ", validators=[DataRequired()], render_kw={'placeholder': '123456'})
+    country = StringField(label="Land", validators=[DataRequired(), Length(max=20)], render_kw={'placeholder': 'Land'})
+    birthday = DateField(label="Geburtsdatum", format="%d.%m.%Y", render_kw={'placeholder': 'dd.mm.yyyy'})
 
     def save(self):
         person = Person(personnumber=customernumgenerator(),
--- DB_APP/Main/models.py
+++ DB_APP/Main/models.py
@@ -5,12 +5,12 @@ from DB_APP import db
 class Person(db.Model):
     __tablename__ = 'person'
     personnumber = db.Column('personennummer', db.Integer, primary_key=True, autoincrement=True)
-    lastname = db.Column('nachname', db.String())
-    firstname = db.Column('vorname', db.String())
-    street = db.Column('strasse', db.String())
-    place = db.Column('ort', db.String())
-    zipcode = db.Column('plz', db.String())
-    country = db.Column('land', db.String())
+    lastname = db.Column('nachname', db.String(20))
+    firstname = db.Column('vorname', db.String(20))
+    street = db.Column('strasse', db.String(30))
+    place = db.Column('ort', db.String(30))
+    zipcode = db.Column('plz', db.Integer())
+    country = db.Column('land', db.String(20))
     birthday = db.Column('geburtsdatum', db.Date())
 
     def __repr__(self):
@@ -20,9 +20,9 @@ class Person(db.Model):
 # DB Tabelle 'Hersteller'
 class Producer(db.Model):
     __tablename__ = 'hersteller'
-    producernumber = db.Column('herstellernummer', db.Integer, primary_key=True, autoincrement=True)
-    producername = db.Column('herstellername', db.String())
-    country = db.Column('land', db.String())
+    producernumber = db.Column('herstellernummer', db.Integer(), primary_key=True, autoincrement=True)
+    producername = db.Column('herstellername', db.String(30))
+    country = db.Column('land', db.String(30))
 
     def __repr__(self):
         return f'{self.producername}'
@@ -45,11 +45,11 @@ class Article(db.Model):
 # DB Tabelle 'Bestellungen'
 class Order(db.Model):
     __tablename__ = 'bestellung'
-    ordernumber = db.Column('bestellnummer', db.Integer, primary_key=True, autoincrement=True)
-    articlenumber = db.Column('artikelnummer', db.Integer, db.ForeignKey(Article.articlenumber))
-    customernumber = db.Column('kundennummer', db.Integer, db.ForeignKey(Person.personnumber))
-    articlequantity = db.Column('artikelanzahl', db.Integer)
-    ordersum = db.Column('bestellsumme', db.Float)
+    ordernumber = db.Column('bestellnummer', db.Integer(), primary_key=True, autoincrement=True)
+    articlenumber = db.Column('artikelnummer', db.Integer(), db.ForeignKey(Article.articlenumber))
+    customernumber = db.Column('kundennummer', db.Integer(), db.ForeignKey(Person.personnumber))
+    articlequantity = db.Column('artikelanzahl', db.Integer())
+    ordersum = db.Column('bestellsumme', db.Float())
 
     article = db.relationship(Article, lazy=True)
     customer = db.relationship(Person, lazy=True)
