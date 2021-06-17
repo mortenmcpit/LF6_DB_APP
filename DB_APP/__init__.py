ca8dc461a592a2cee246f3c6bd8c2fd0a508ef41
fschumacher <felix.schumacher@iee.fraunhofer.de>
2021-06-17 08:21:53 +0200

    static bug fix

--- DB_APP/__init__.py
+++ DB_APP/__init__.py
@@ -9,15 +9,15 @@ db = SQLAlchemy()
 
 
 def create_app(config_class=Config):
-    # Setting the 'templates' directory as default template-directoy for Flask App
+    # Setting the 'templates' and 'static' directory as default template-directoy for Flask App
     template_dir = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
     template_dir = os.path.join(template_dir, 'templates')
-
-    # For testing purposes
-    # print(template_dir)
+    static_dir = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+    static_dir = os.path.join(static_dir, 'static')
 
     app = Flask(__name__,
-                template_folder=template_dir
+                template_folder=template_dir,
+                static_folder=static_dir
                 )
     app.config.from_object(Config)
 
